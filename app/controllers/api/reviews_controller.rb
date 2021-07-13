5adc075c29b9391fe25310d24ab83f81d34f799b
david <davidlai9@gmail.com>
2021-07-13 12:13:36 -0700

    created controllers and routes for reviews

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,7 +1,46 @@
 class Api::ReviewsController < ApplicationController
 
     def index 
-        @reviews = Review.all.find_by 
+        @reviews = Review.all.where(business_id: params[:business_id])
+        render :index
+    end
+
+    def show 
+        @review = Review.find_by(id: params[:id])
+        render :show
+    end
+
+    def create 
+        @review = Review.new(review_params)
+        if @review.save 
+            render :show 
+        else
+            render json: @review.errors.full_messages, status: 422
+        end
+    end
+
+    def update 
+        @review = Review.find_by(id: params[:id])
+        if @review.update(review_params)
+            render: show 
+        else
+            render json: ["Cannot update reviews"] 
+        end
+    end
+
+    def destroy 
+        @review = Review.find_by(id: params[:id])
+        if current_user.id == @review.user_id 
+            @review.destroy
+        else
+            render json: ["Cannot delete review"]
+        end
+    end
+
+    private 
+
+    def review_params
+        params.require(:review).permit(:rating, :message, :business_id, :user_id)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy]
-    resources :businesses, only: [:show, :index]
+    resources :businesses, only: [:show, :index] do
+      resources :reviews, only: [:show, :create, :index, :update, :destroy]
+    end
   end
 
   root to: "static_pages#root"
