c11d2968983b38011b90897dd03da20a5806287f
david <davidlai9@gmail.com>
2021-07-17 19:50:02 -0700

    submitting review form

--- frontend/components/reviews/review_form.jsx
+++ frontend/components/reviews/review_form.jsx
@@ -25,7 +25,7 @@ class ReviewForm extends React.Component {
     e.preventDefault();
     const review = Object.assign({}, this.state);
     this.props
-      .createReview(review, this.props.business.businessId)
+      .createReview(review, this.props.business.id)
       .then(() =>
         this.props.history.push(`/businesses/${this.props.business.id}`)
       );
@@ -46,7 +46,6 @@ class ReviewForm extends React.Component {
   renderErrors() {
     let error;
     if (this.props.errors.length !== 0) {
-      debugger;
       error = this.props.errors[0];
       if (error.includes("Message")) {
         error = "Please explain your review";
@@ -55,13 +54,80 @@ class ReviewForm extends React.Component {
           "Please select a rating for this business.";
       }
     }
+    return (
+        <p className="review-form-error">{error}</p>
+    )
   }
 
   render() {
     if (!this.props.business) return null;
     return (
       <>
-        <p>hello</p>
+        <div className="review-form-container">
+          <h3>{this.props.business.name}</h3>
+          <div className="review-form">
+            <form onSubmit={this.handleSubmit} className="review-form-box">
+              <div className="review-content-container">
+                <label>
+                  Not Good
+                  <input
+                    name="rating"
+                    type="radio"
+                    // checked={this.state.rating === 1}
+                    onChange={this.handleRating}
+                    value="1"
+                  />
+                </label>
+                <label>
+                  Could've been better
+                  <input
+                    name="rating"
+                    type="radio"
+                    // checked={this.state.rating === 2}
+                    onChange={this.handleRating}
+                    value="2"
+                  />
+                </label>
+                <label>
+                  OK
+                  <input
+                    name="rating"
+                    type="radio"
+                    // checked={this.state.rating === 3}
+                    onChange={this.handleRating}
+                    value="3"
+                  />
+                </label>
+                <label>
+                  Good
+                  <input
+                    name="rating"
+                    type="radio"
+                    // checked={this.state.rating === 4}
+                    onChange={this.handleRating}
+                    value="4"
+                  />
+                </label>
+                <label>
+                  Great
+                  <input
+                    name="rating"
+                    type="radio"
+                    // checked={this.state.rating === 5}
+                    onChange={this.handleRating}
+                    value="5"
+                  />
+                </label>
+              </div>
+              <div>
+                  <input type="text-area" className="review-message-body" onChange={this.handleBody} value={this.state.body} placeholder="Please write a message"/>
+              </div>
+              <div>
+                  <button className="review-submit-button">Post Review</button>
+              </div>
+            </form>
+          </div>
+        </div>
       </>
     );
   }
--- frontend/components/reviews/review_index.jsx
+++ frontend/components/reviews/review_index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { BsThreeDots } from "react-icons/bs";
 
 class ReviewsIndex extends React.Component {
   constructor(props) {
@@ -39,98 +38,11 @@ class ReviewsIndex extends React.Component {
     let { reviews } = this.props;
     return (
       <>
-        <div className="reviews-wrapper">
-          {reviews.map((review) => {
-            let date = review.created_at.slice(0, 10);
-            let styleStars = {
-              width: "108px",
-              height: "4px",
-              display: "inlineBlock",
-              verticalAlign: "middle",
-              background:
-                'url("https://s3-media0.fl.yelpcdn.com/assets/public/stars_v2.yji-52d3d7a328db670d4402843cbddeed89.png")',
-              backgroundPosition: "0 -500px",
-            };
-            if (review.rating === 5) {
-              styleStars = {
-                width: "108px",
-                height: "4px",
-                display: "inlineBlock",
-                verticalAlign: "middle",
-                background:
-                  'url("https://s3-media0.fl.yelpcdn.com/assets/public/stars_v2.yji-52d3d7a328db670d4402843cbddeed89.png")',
-                backgroundPosition: "0 -500px",
-              };
-            } else if (review.rating === 4) {
-              styleStars = {
-                width: "108px",
-                height: "4px",
-                display: "inlineBlock",
-                verticalAlign: "middle",
-                background:
-                  'url("https://s3-media0.fl.yelpcdn.com/assets/public/stars_v2.yji-52d3d7a328db670d4402843cbddeed89.png")',
-                backgroundPosition: "0 -480px",
-              };
-            } else if (review.rating === 3) {
-              styleStars = {
-                width: "108px",
-                height: "4px",
-                display: "inlineBlock",
-                verticalAlign: "middle",
-                background:
-                  'url("https://s3-media0.fl.yelpcdn.com/assets/public/stars_v2.yji-52d3d7a328db670d4402843cbddeed89.png")',
-                backgroundPosition: "0 -440px",
-              };
-            } else if (review.rating === 2) {
-              styleStars = {
-                width: "108px",
-                height: "4px",
-                display: "inlineBlock",
-                verticalAlign: "middle",
-                background:
-                  'url("https://s3-media0.fl.yelpcdn.com/assets/public/stars_v2.yji-52d3d7a328db670d4402843cbddeed89.png")',
-                backgroundPosition: "0 -400px",
-              };
-            } else if (review.rating === 1) {
-              styleStars = {
-                width: "108px",
-                height: "4px",
-                display: "inlineBlock",
-                verticalAlign: "middle",
-                background:
-                  'url("https://s3-media0.fl.yelpcdn.com/assets/public/stars_v2.yji-52d3d7a328db670d4402843cbddeed89.png")',
-                backgroundPosition: "0 -360px",
-              };
-            }
-            let stars = <div style={styleStars}></div>;
-            return (
-              <div className="reviews-container">
-                <div className="review-user-info">
-                  <div>{review.author.first_name}</div>
-                  <div>
-                    <button
-                      className="update-delete-button"
-                      onClick={this.toggleDrop}
-                    >
-                      <BsThreeDots />
-                    </button>
-                  </div>
-                  {this.state.dropDown ? (
-                    <div>
-                      <button>Edit review</button>
-                      <button>Delete review</button>
-                    </div>
-                  ) : null}
-                </div>
-                <div className="rating-container">
-                  {stars}
-                  <div className="date">{date}</div>
-                </div>
-                <div className="review-body">{review.body}</div>
-              </div>
-            );
-          })}
-        </div>
+       <div>
+           {reviews.map((review) => (
+               <p>{review}</p>
+           ))}
+       </div>
       </>
     );
   }
