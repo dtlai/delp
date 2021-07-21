b21adae0054c94f37223426e4c14d7c1a38542bb
david <davidlai9@gmail.com>
2021-07-20 17:39:53 -0700

    added categories models

--- app/models/business_category.rb
+++ app/models/business_category.rb
@@ -1,2 +1,9 @@
 class BusinessCategory < ApplicationRecord
+    belongs_to :business,
+        class_name: 'Business', 
+        foreign_key: :business_id
+
+    belongs_to :category,
+        class_name: 'Category',
+        foreign_key: :category_id
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,11 @@
 class Category < ApplicationRecord
+    validates :category, presence: true 
+
+    has_many :businesses_categories,
+        class_name: 'BusinessCategory',
+        foreign_key: :category_id
+
+    has_many :businesses,
+        through: :business_categories,
+        source: :business 
 end
