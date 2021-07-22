ea7ff20240a3a9094d182585ce832105f3bd4b48
david <davidlai9@gmail.com>
2021-07-22 14:49:01 -0700

    styling navbar

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,25 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-
-// const NavBar = ({ currentUser, logout }) => {
-//     const sessionLinks = () => (
-//         <nav className="login-signup">
-//             <Link to="/login" className="login-greeting">Login</Link>
-//             <Link to="/signup" className="signup-greeting">Sign up</Link>
-//         </nav>
-//     );
-//     const personalGreeting = () => (
-//       <hgroup className="header-group">
-//         <h2 className="header-name">Hi, {currentUser.first_name}!</h2>
-//         <button className="header-button" onClick={logout}>
-//           Log Out
-//         </button>
-//       </hgroup>
-//     );
-
-//     return currentUser ? personalGreeting() : sessionLinks();
-// };
+import SearchBarContainer from '../search_form/search_form_container';
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -124,6 +105,9 @@ class NavBar extends React.Component {
       <>
         <div className="nav-bar">
           <div className="nav-links">{sessionButtons}</div>
+        </div>
+        <div className="searchbar-container">
+          <SearchBarContainer />
         </div>
       </>
     );
