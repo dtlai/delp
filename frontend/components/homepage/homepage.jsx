61a948db7b787e09cbfaba82eeead4968198479a
david <davidlai9@gmail.com>
2021-07-28 16:47:44 -0700

    fixing header homepage

--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -4,7 +4,7 @@ import NavBarContainer from '../navbar/navbar_container'
 import BusinessContainer from '../business_page/business_container'
 import BusinessIndexContainer from '../business_page/business_index_container'
 import SearchBarContainer from '../search_form/search_form_container'
-// import { FaLinkedin, FaGithub, FaAngellist } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaAngellist } from "react-icons/fa";
 
 class Homepage extends React.Component {
     constructor(props) {
@@ -18,31 +18,31 @@ class Homepage extends React.Component {
               <div className="hero-content">
                 <div className="nav-bar">
                   <div className="header-nav">
-                    <div className="about-me-links">
+                    <div className="other-proj-links">
                       <a
-                        href="http://davidtlai.com/"
+                        href="mailto:davidlai9@gmail.com"
                         target="_blank"
                         rel="noreferrer"
                       >
-                        David Lai Portolio
+                        Write an Email
                       </a>
                     </div>
-                    <div className="about-me-links">
+                    <div className="other-proj-links">
                       <a
-                        href="https://www.linkedin.com/in/davidlai9/"
+                        href="https://ridealong-app.herokuapp.com/"
                         target="_blank"
                         rel="noreferrer"
                       >
-                        LinkedIn
+                        RideAlong
                       </a>
                     </div>
-                    <div className="about-me-links">
+                    <div className="other-proj-links">
                       <a
-                        href="https://github.com/dtlai"
+                        href="https://dtlai.github.io/stocktok/"
                         target="_blank"
                         rel="noreferrer"
                       >
-                        Github
+                        StockTok
                       </a>
                     </div>
                   </div>
@@ -60,6 +60,21 @@ class Homepage extends React.Component {
                   <div className="search-bar-form">
                     <SearchBarContainer formType={this.props.formType} />
                   </div>
+                  <div className="about-me-container">
+                    <div className="about-links">
+                      <a
+                        href="https://www.linkedin.com/in/davidlai9/"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <FaLinkedin className="about-us-icon" size={30} color="white"/>
+                      </a>
+                    </div>
+                  </div>
+                  <div className="photo-creds">
+                    <h3>龍天門</h3>
+                    <p>Photo by トモ T.</p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -68,7 +83,6 @@ class Homepage extends React.Component {
                 <img src="https://delp-seeds.s3.us-west-1.amazonaws.com/briefcase.png" />
               </Link> */}
               Find the Best Restaurants in Town
-
             </div>
           </>
         );
