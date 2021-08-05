6eb93c26c449631979bc3878e279a7c71204fa71
david <davidlai9@gmail.com>
2021-08-05 11:59:56 -0700

    footer links

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,13 +1,64 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 const Footer = () => (
   <>
     <div className="footer-container">
-      <img
-        src="https://s3-media0.fl.yelpcdn.com/assets/public/footer_cityscape.yji-573fa19c843556eac5a998fc6d1f80f8.png"
-        alt="none"
-      />
+      <div className="footer-titles">
+        <h3>About</h3>
+        <h3>Projects</h3>
+        <h3>Get Started</h3>
+      </div>
+      <div className="footer-links">
+        <div className="about-container">
+          <a
+            href="https://www.linkedin.com/in/davidlai9/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            LinkedIn
+          </a>
+          <a
+            href="https://www.linkedin.com/in/davidlai9/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          <a
+            href="https://www.linkedin.com/in/davidlai9/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            AngelList
+          </a>
+        </div>
+
+        <div>
+            
+        </div>
+
+        <div className="get-started-container">
+          <Link className="get-started-link" to="/login">
+            Login
+          </Link>
+          <Link className="get-started-link" to="/signup">
+            Sign Up
+          </Link>
+        </div>
+      </div>
+
+      <div className="footer-img">
+        <img
+          src="https://s3-media0.fl.yelpcdn.com/assets/public/footer_cityscape.yji-573fa19c843556eac5a998fc6d1f80f8.png"
+          alt="none"
+        />
+      </div>
+      <div className="copyright-container">
+        <p>
+          Copyright &copy; 2021 Delp Inc.
+        </p>
+      </div>
     </div>
   </>
 );
