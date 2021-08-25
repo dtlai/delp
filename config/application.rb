b9c086f45c3f56331ff765c7a03df47b87a020c7
david <davidlai9@gmail.com>
2021-08-25 11:02:50 -0700

    added another line to application rb

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,8 @@ module Delp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    
-    config.assets.initialize_on_precompile = false
+
+    config.assets.initialize_on_precompile = ["application.js", "application.css"]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
